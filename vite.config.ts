df6d63ade3512a2c419cd9190367ddaea926ce43
zcreeto <167029918+zcreeto@users.noreply.github.com>
2025-11-23 22:14:12 +0530

    Inject SpeedInsights in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
