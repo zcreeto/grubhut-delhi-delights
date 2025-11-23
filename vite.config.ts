46ad3e36a9fda19bce08ababaf848a6aa0ef8777
zcreeto <167029918+zcreeto@users.noreply.github.com>
2025-11-23 20:27:21 +0530

    Add SpeedInsights import to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
