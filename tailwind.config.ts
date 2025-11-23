c951506623df39a7c331ed2783a92626acc70b51
zcreeto <167029918+zcreeto@users.noreply.github.com>
2025-11-23 22:16:50 +0530

    Remove speed insights injection from config
    Removed the import and invocation of injectSpeedInsights.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from "tailwindcss";
-import { injectSpeedInsights } from '@vercel/speed-insights';
 
-injectSpeedInsights();
 export default {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
